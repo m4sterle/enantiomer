c319df6293769dbfaf5084219fc54d16dc424bf0
Master Le <dev@masterle.xyz>
2024-05-30 19:47:33 -0500

    Create commands.js

---
+++ js/commands.js
@@ -0,0 +1,83 @@
+var ddxsquad = "https://www.playbook.com/s/master-le-inc/ddxsquad";
+var masterle = "https://dev.masterle.xyz/about";
+var twitter = "https://www.twitter.com/notmasterle/";
+var video = "https://www.youtube.com/watch?v=UieeBxPVvHM";
+var password = "yogitron";
+var github = "https://github.com/ddxsquad/";
+var email = 'mailto:iam@masterle.xyz';
+
+whois = [
+  "<br>",
+  "Sop!👋",
+  "This is the ddxSQUAD aka Detective Le.... and Y0G1TR0N... oh, ya Master Le too.",
+  "So.... come here to take a gander at the ddxCLUE terminal, eh? DARE to see more?",
+  "<br>",
+  "Well, go on... continue to try out those cmds...",
+  "<br>"
+];
+
+whoami = [
+  "<br>",
+  "DAT A GOOD QUESTION! WHO ARE YUS????????? WOT R U DOING IN ME TERMINAL?",
+  "<br>"
+];
+
+social = [
+  "<br>",
+  'twitter        <a href="' + twitter + '" target="_blank">twitter/notmasterle' + '</a>',
+  'github         <a href="' + github + '" target="_blank">github/ddxsquad' + "</a>",
+  "<br>"
+];
+
+secret = [
+  "<br>",
+  '<span class="command">sudo</span>           Only use if you\'re admin',
+  "<br>"
+];
+
+projects = [
+  "<br>",
+  "Still curating... in other words, they are scattered all over the web.", 
+  "I may or may not even know where they are...But... for now.... you can meet...",
+  `The <a href='${masterle}' target='_blank'>master</a> ...if you dare...`,
+  "<br>",
+];
+
+help = [
+  "<br>",
+  '<span class="command">whois</span>          Who is behind all this!?',
+  '<span class="command">whoami</span>         Who are you!!???',
+  '<span class="command">squad</span>          Who tf is the ddxSQUAD?',
+  '<span class="command">video</span>          View a revolutionary video',
+  '<span class="command">social</span>         Display social networks',
+  '<span class="command">secret</span>         Find the password',
+  '<span class="command">projects</span>       View coding projects',
+  '<span class="command">history</span>        View command history',
+  '<span class="command">help</span>           You obviously already know what this does',
+  '<span class="command">email</span>          Do not email me',
+  '<span class="command">clear</span>          Clear terminal',
+  '<span class="command">banner</span>         Display the header',
+  "<br>",
+];
+
+banner = [
+  '<span class="index">PS C:/usrs/detectivele> git init ddxTERMINAL</span>',
+  '<span class="index">🤖 Y0G1-TR0N: enantiomers... initating....</span>',
+  '<span class="index">🤖 Y0G1-TR0N: bruff-bruff...where is... ball...</span>',
+  "<br>",
+  "<br>",
+  "<br>",
+  '<span class="index">🤖 Y0G1-TR0N: .......................enantiomers init complete!</span>',
+  "<br>",
+"   _______  _        _______  _       __________________ _______  _______  _______  _______ ", 
+"  (  ____ \( (    /|(  ___  )( (    /|\__   __/\__   __/(  ___  )(       )(  ____ \(  ____ ) ",
+"  | (    \/|  \  ( || (   ) ||  \  ( |   ) (      ) (   | (   ) || () () || (    \/| (    )| ",
+"  | (__    |   \ | || (___) ||   \ | |   | |      | |   | |   | || || || || (__    | (____)| ",
+"  |  __)   | (\ \) ||  ___  || (\ \) |   | |      | |   | |   | || |(_)| ||  __)   |     __) ",
+"  | (      | | \   || (   ) || | \   |   | |      | |   | |   | || |   | || (      | (\ (    ",
+"  | (____/\| )  \  || )   ( || )  \  |   | |   ___) (___| (___) || )   ( || (____/\| ) \ \__ ",
+"  (_______/|/    )_)|/     \||/    )_)   )_(   \_______/(_______)|/     \|(_______/|/   \__/ ",
+  " 🤖 Y0G1-TR0N: System init done.",
+  '<span class="color2"> welcome, bitch.</span>',
+  "<span class=\"color2\">well, go on.... I know u don't know what da hail going on. just type</span> <span class=\"command\">'help'</span><span class=\"color2\">.</span>",
+];
